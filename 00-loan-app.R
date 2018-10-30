843f21b0525c77aea61327c49a95c9e2892a4bfe
Jesse <16379819+JesseVent@users.noreply.github.com>
2018-10-31 00:00:32 +1030

    Update 00-loan-app.R

--- 00-loan-app.R
+++ 00-loan-app.R
@@ -1,6 +1,10 @@
+library(tidyverse)
 library(xesreadR)
 library(processanimateR)
 library(processmapR)
+
+#   ____________________________________________________________________________
+#   Loan Application Event Logs                                             ####
 
 loan  <- xesreadR::read_xes("https://solutiondesign.io/blog/data/loan_app_data.xes")
 
@@ -11,3 +15,17 @@ model <- DiagrammeR::add_global_graph_attrs(graph,
             attr_type = c("graph", "graph", "graph"))
 
 animate_process(loan, model, mode = "relative", duration = 240)
+
+
+#   ____________________________________________________________________________
+#   Traffic Fines Event Logs                                                ####
+
+fines   <- eventdataR::traffic_fines
+
+graph_f <- processmapR::process_map(fines, type_edges = performance(mean), render = F)
+model_f <- DiagrammeR::add_global_graph_attrs(graph_f,
+            attr      = c("layout", "rankdir", "splines"),
+            value     = c("dot", "LR", "ortho"),
+            attr_type = c("graph", "graph", "graph"))
+
+animate_process(fines, model_f, mode = "relative", duration = 240)
