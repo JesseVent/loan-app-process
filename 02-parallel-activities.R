1bed4aab2adb03198242ae12676a2317e443ef6b
Jesse <16379819+JesseVent@users.noreply.github.com>
2018-11-20 01:50:06 +1030

    Enhanced overlapping process handling

--- 02-parallel-activities.R
+++ 02-parallel-activities.R
@@ -6,101 +6,94 @@ library(processanimateR)
 library(processmapR)
 library(bupaR)
 
-#   ____________________________________________________________________________
-#   Ranks sequence of overlapping events                                    ####
+remove_activity <- function(activity, event_log) {
+  elog  <- event_log %>% as.data.frame() %>%
+  select(case_id, rank, activity_id, activity_instance, resource_id, lifecycle_id, timestamp) %>%
+  group_by(case_id, rank, activity_id, activity_instance, resource_id) %>%
+  spread(lifecycle_id, timestamp) %>%
+  arrange(case_id, rank) %>%
+  ungroup() %>%
+  group_by(case_id) %>%
+  mutate(next_start = lead(start))
 
-rank_sequence <- function(x) {
-    iterations <- seq_len(nrow(x))
-    rank       <- as.numeric(1)
-    x$lag      <- lag(x$overlap)
-    na         <- which(is.na(x$lag))
-    x$lag[na]  <- x$overlap[na]
-  for (i in iterations) {
-      if (x$overlap[i] != x$lag[i]) {
-        rank[i] <- max(rank) + 1
-      } else {
-        rank[i] <- max(rank)
-        }
-   }
-  return(rank)
+  elog$difference <- if_else(is.na(elog$next_start),
+        difftime(elog$end, lag(elog$end), units = "mins"),
+        difftime(elog$next_start, elog$start, units = "mins")) %>% round(digits = 1)
+  return(elog)
 }
 
 #   ____________________________________________________________________________
 #   Loan Application BPI17 Event Logs                                       ####
 
-loan  <- xesreadR::read_xes("https://solutiondesign.io/blog/data/loan_app_data.xes")
+loan <- xesreadR::read_xes("loan_data_BPI17.xes")
 
-log_df <- loan %>% as.data.frame() %>%
-  select(CASE_concept_name, activity_instance_id, activity_id, resource_id, lifecycle_id, timestamp) %>%
+log  <- loan %>% as.data.frame() %>%
+  select(CASE_concept_name, activity_instance_id, activity_id, resource_id,
+         lifecycle_id, timestamp) %>%
   group_by(CASE_concept_name, activity_instance_id, activity_id, resource_id) %>%
   spread(lifecycle_id, timestamp) %>%
   arrange(CASE_concept_name, activity_instance_id, starttimestamp) %>%
   ungroup()
 
-names(log_df) <- c("case_id", "activity_instance_id", "activity_id", "resource_id","end","start")
-log_df <- log_df[c("case_id", "activity_instance_id", "activity_id", "resource_id","start","end")]
-log_df$activity_id <- as.character(log_df$activity_id)
-log_df$resource_id <- as.character(log_df$resource_id)
+names(log)      <- c("case_id", "activity_instance_id", "activity_id", "resource_id","end","start")
+log$activity_id <- as.character(log$activity_id)
+log$resource_id <- as.character(log$resource_id)
 
 #   ____________________________________________________________________________
-#   Identify overlapping activities                                         ####
+#   Merge activities starting at same time together                         ####
+log <- log %>% group_by(case_id, start) %>%
+  summarise(activity_instance_id = min(activity_instance_id),
+            activity_id = paste(activity_id, collapse = " & "),
+            resource_id = paste(unique(resource_id), collapse = " & "),
+            end = max(end))
 
-log_df <- log_df %>% group_by(case_id) %>%
-  mutate(fstart = lead(start),
-         fend   = lead(end))
-
-log_df$fstart    <- ifelse(is.na(log_df$fstart), log_df$start, log_df$fstart) %>% as_datetime()
-log_df$fend      <- ifelse(is.na(log_df$fend), log_df$end, log_df$fend) %>% as_datetime()
-log_df$period    <- interval(log_df$start, log_df$end)
-log_df$fperiod   <- interval(log_df$fstart, log_df$fend)
-log_df$overlap   <- int_overlaps(log_df$period, log_df$fperiod)
-log_df$aligns    <- int_aligns(log_df$period, log_df$fperiod)
-
-#   ____________________________________________________________________________
-#   Rank sequence of activities                                             ####
-
-log_df$rank <- log_df %>%
-  split(.$case_id) %>%
-  purrr::map(rank_sequence) %>%
-  unlist()
+log$start    <- as.numeric(log$start)
+log$end      <- as.numeric(log$end)
 
 #   ____________________________________________________________________________
 #   Combine overlapping activities into new activity                        ####
 
-log_columns <- c("case_id", "activity_instance_id", "activity_id", "resource_id","start","end","rank")
-
-event <- log_df %>% select(log_columns) %>%
-  group_by(case_id, rank) %>%
+log_df       <- log %>% arrange(start) %>%
+  group_by(case_id, rank = cumsum(cummax(lag(end, default = first(end))) < start)) %>%
   summarise(activity_instance_id = min(activity_instance_id),
             activity_id = paste(activity_id, collapse = " & "),
-            resource_id = paste(resource_id, collapse = " & "),
-            start = min(start),
-            end = max(start))
+            resource_id = paste(unique(resource_id), collapse = " & "),
+            start = first(start),
+            end = max(end))
+
+log_df$start <- as.POSIXct(log_df$start, origin = "1970-01-01", tz = "UTC")
+log_df$end   <- as.POSIXct(log_df$end,   origin = "1970-01-01", tz = "UTC")
+
+log_columns  <- c("case_id", "activity_instance_id", "activity_id", "resource_id",
+                  "start", "end", "rank")
 
 #   ____________________________________________________________________________
 #   Convert to eventlog class                                               ####
 
-event <- event %>% ungroup() %>% mutate(activity_instance = 1:nrow(.)) %>%
-  gather(status, timestamp, start, end) %>%
-  arrange(case_id, rank, desc(status)) %>%
+event <- log_df %>% ungroup() %>%
+  mutate(activity_instance = 1:nrow(.)) %>%
+  gather(lifecycle_id, timestamp, start, end) %>%
+  arrange(case_id, rank, desc(lifecycle_id)) %>%
   filter(!is.na(timestamp)) %>%
-  eventlog(
-    case_id              = "case_id",
+  eventlog(case_id       = "case_id",
     activity_id          = "activity_id",
     activity_instance_id = "activity_instance",
-    lifecycle_id         = "status",
+    lifecycle_id         = "lifecycle_id",
     timestamp            = "timestamp",
-    resource_id          = "resource_id"
-  )
+    resource_id          = "resource_id")
 
 #   ____________________________________________________________________________
 #   Generate animated process map                                           ####
 
-events <- event %>% filter_activity_frequency(percentage = 0.5)
-graph <- processmapR::process_map(events, type_edges = performance(mean, units = "days"), render = F)
-model <- DiagrammeR::add_global_graph_attrs(graph,
-                                            attr      = c("layout", "rankdir", "splines"),
-                                            value     = c("dot", "LR", "ortho"),
-                                            attr_type = c("graph", "graph", "graph"))
+
+events <- event %>% filter_activity_frequency(percentage = 0.7)
+graph  <- processmapR::process_map(events, type_edges = performance(mean, units = "days"), render = F)
+model  <- DiagrammeR::add_global_graph_attrs(graph,
+                                            attr      = c("rankdir", "splines"),
+                                            value     = c("LR", "ortho"),
+                                            attr_type = c("graph", "graph"))
 
 animate_process(events, model, mode = "relative", duration = 240)
+
+
+df <- precedence_matrix(loan, type = "relative_consequent") %>% plot()
---
+++ 03-parallel-activities-legacy.R
@@ -0,0 +1,106 @@
+library(tidyverse)
+library(lubridate)
+library(purrr)
+library(xesreadR)
+library(processanimateR)
+library(processmapR)
+library(bupaR)
+
+#   ____________________________________________________________________________
+#   Ranks sequence of overlapping events                                    ####
+
+rank_sequence <- function(x) {
+    iterations <- seq_len(nrow(x))
+    rank       <- as.numeric(1)
+    x$lag      <- lag(x$overlap)
+    na         <- which(is.na(x$lag))
+    x$lag[na]  <- x$overlap[na]
+  for (i in iterations) {
+      if (x$overlap[i] != x$lag[i]) {
+        rank[i] <- max(rank) + 1
+      } else {
+        rank[i] <- max(rank)
+        }
+   }
+  return(rank)
+}
+
+#   ____________________________________________________________________________
+#   Loan Application BPI17 Event Logs                                       ####
+
+loan      <- xesreadR::read_xes("loan_data_BPI17.xes")
+
+log_df <- loan %>% as.data.frame()
+  select(CASE_concept_name, activity_instance_id, activity_id, resource_id, lifecycle_id, timestamp) %>%
+  group_by(CASE_concept_name, activity_instance_id, activity_id, resource_id) %>%
+  spread(lifecycle_id, timestamp) %>%
+  arrange(CASE_concept_name, activity_instance_id, starttimestamp) %>%
+  ungroup()
+
+names(log_df) <- c("case_id", "activity_instance_id", "activity_id", "resource_id","end","start")
+log_df <- log_df[c("case_id", "activity_instance_id", "activity_id", "resource_id","start","end")]
+log_df$activity_id <- as.character(log_df$activity_id)
+log_df$resource_id <- as.character(log_df$resource_id)
+
+#   ____________________________________________________________________________
+#   Identify overlapping activities                                         ####
+
+log_df <- log_df %>% group_by(case_id) %>%
+  mutate(fstart = lead(start),
+         fend   = lead(end))
+
+log_df$fstart    <- ifelse(is.na(log_df$fstart), log_df$start, log_df$fstart) %>% as_datetime()
+log_df$fend      <- ifelse(is.na(log_df$fend), log_df$end, log_df$fend) %>% as_datetime()
+log_df$period    <- interval(log_df$start, log_df$end)
+log_df$fperiod   <- interval(log_df$fstart, log_df$fend)
+log_df$overlap   <- int_overlaps(log_df$period, log_df$fperiod)
+log_df$aligns    <- int_aligns(log_df$period, log_df$fperiod)
+
+#   ____________________________________________________________________________
+#   Rank sequence of activities                                             ####
+
+log_df$rank <- log_df %>%
+  split(.$case_id) %>%
+  purrr::map(rank_sequence) %>%
+  unlist()
+
+#   ____________________________________________________________________________
+#   Combine overlapping activities into new activity                        ####
+
+log_columns <- c("case_id", "activity_instance_id", "activity_id", "resource_id","start","end","rank")
+
+event <- log_df %>% select(log_columns) %>%
+  group_by(case_id, rank) %>%
+  summarise(activity_instance_id = min(activity_instance_id),
+            activity_id = paste(activity_id, collapse = " & "),
+            resource_id = paste(resource_id, collapse = " & "),
+            start = min(start),
+            end = max(start))
+
+#   ____________________________________________________________________________
+#   Convert to eventlog class                                               ####
+
+event <- event %>% ungroup() %>% mutate(activity_instance = 1:nrow(.)) %>%
+  gather(status, timestamp, start, end) %>%
+  arrange(case_id, rank, desc(status)) %>%
+  filter(!is.na(timestamp)) %>%
+  eventlog(
+    case_id              = "case_id",
+    activity_id          = "activity_id",
+    activity_instance_id = "activity_instance",
+    lifecycle_id         = "status",
+    timestamp            = "timestamp",
+    resource_id          = "resource_id"
+  )
+
+#   ____________________________________________________________________________
+#   Generate animated process map                                           ####
+
+events <- event %>% filter_activity_frequency(percentage = 0.5)
+graph <- processmapR::process_map(events, type_edges = performance(mean, units = "days"), render = F)
+model <- DiagrammeR::add_global_graph_attrs(graph,
+                                            attr      = c("layout", "rankdir", "splines"),
+                                            value     = c("dot", "LR", "ortho"),
+                                            attr_type = c("graph", "graph", "graph"))
+
+animate_process(events, model, mode = "relative", duration = 240)
